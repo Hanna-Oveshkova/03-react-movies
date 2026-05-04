2090e750291bff734df55bfb3d681340bc801db3
Hanna-Oveshkova <ganusya.ua@gmail.com>
2026-05-04 13:03:18 +0300

    fix env

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -9,6 +9,7 @@ interface FetchMoviesResponse {
 // const TOKEN = import.meta.env.VITE_TMDB_TOKEN;
 
 export const fetchMovies = async (query: string): Promise<Movie[]> => {
+  console.log("TOKEN:", import.meta.env.VITE_TMDB_TOKEN);
   const response = await axios.get<FetchMoviesResponse>(
     "https://api.themoviedb.org/3/search/movie",
     {
